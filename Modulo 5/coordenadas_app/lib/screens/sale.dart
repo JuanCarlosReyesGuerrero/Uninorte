631a9a011fb606e8653f76f4b6aef9a10a0121bf
Juan Carlos Reyes <ingreyesguerrero@gmail.com>
2023-09-29 17:45:14 -0500

    Base de datos
    Base de datos

--- Modulo 5/coordenadas_app/lib/screens/sale.dart
+++ Modulo 5/coordenadas_app/lib/screens/sale.dart
@@ -1,5 +1,7 @@
 import 'package:coordenadas_app/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Sale extends StatefulWidget {
   @override
@@ -7,6 +9,9 @@ class Sale extends StatefulWidget {
 }
 
 class _SaleState extends State<Sale> {
+  //final Stream<QuerySnapshot> _cityStream =
+  //FirebaseFirestore.instance.collection('venta').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +19,27 @@ class _SaleState extends State<Sale> {
       appBar: AppBar(
         title: Text("Estado Venta"),
       ),
-      body: Center(child: Text("Estado Venta")),
+      //body: Center(child: Text("Estado Venta")),
+      body: StreamBuilder<QuerySnapshot>(
+          //stream: _cityStream,
+          stream: FirebaseFirestore.instance.collection('venta').snapshots(),
+          //stream: FirebaseFirestore.instance.collection('venta').doc('UID').get(),
+
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading...');
+            }
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                final data = document.data()! as Map<String, dynamic>;
+                return ListTile(leading: Text(data['estado'].toString()));
+              }).toList(),
+            );
+          }),
     );
   }
 }
